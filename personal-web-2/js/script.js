59a41a0967d56db37dc6ac78a94df0cb0a23237c
uwan <uwanmaharaya026@gmail.com>
2023-09-19 11:29:16 +0700

    tambah multiple-text

--- personal-web-2/js/script.js
+++ personal-web-2/js/script.js
@@ -47,3 +47,12 @@ ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .project-container, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+/* typed js */
+const typed = new Typed('.multiple-text', {
+  strings: ['Web Designer, Photographer, Videographer'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 100,
+  loop: true,
+});
